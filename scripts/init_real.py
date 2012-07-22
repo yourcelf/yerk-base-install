d3ddba9dc869cd9c6eaf60aa44ed50c53fe52c89
John Nishinaga <jingoro@casa-z.org>
2012-07-22 11:53:42 -0400

    Rename firewall scripts

--- scripts/init_real.py
+++ scripts/init_real.py
@@ -4,6 +4,6 @@ from common import *
 
 script("system_update.py")
 
-script("install_firewall.py")
-script("configure_firewall_real.py")
-script("finalize_firewall.py")
+script("firewall_1_install.py")
+script("firewall_3_configure_real.py")
+script("firewall_5_finalize.py")

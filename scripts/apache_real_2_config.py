2504d818471fba6b42def6403f396e11f99ab2e0
Charlie DeTar <cfd@media.mit.edu>
2012-07-22 21:30:12 -0600

    Adding apache

---
+++ scripts/apache_real_2_config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from common import sh
+
+sh("""
+
+set -x
+set -e
+
+rm -rf /etc/apache2/sites-enabled/
+rm -rf /etc/apache2/sites-available/
+rm -rf /etc/apache2/sites-includes/
+
+ln -s /yerk-data/etc/apache2/sites-enabled/ /etc/apache2/sites-enabled/
+ln -s /yerk-data/etc/apache2/sites-available/ /etc/apache2/sites-available/
+ln -s /yerk-data/etc/apache2/site-includes/ /etc/apache2/site-includes/
+
+a2enmod headers
+a2enmod ssl
+
+/etc/init.d/apache2 restart
+""")
